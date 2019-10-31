a466c41e08899881defb15b3cd6231bee2cc77b5
Tomáš Lipovský <tomas@lipovo.cz>
2019-10-31 18:02:10 +0100

    Fix loading nominatim data folder as volume

--- 3.3/init.sh
+++ 3.3/init.sh
@@ -1,19 +1,18 @@
 OSMFILE=$1
-PGDIR=$2
-THREADS=$3
+THREADS=$2
 
-mkdir -p /data/$PGDIR && \
+mkdir -p /postgresdata && \
 
-chown postgres:postgres /data/$PGDIR && \
+chown postgres:postgres /postgresdata && \
 
-export  PGDATA=/data/$PGDIR  && \
-sudo -u postgres /usr/lib/postgresql/11/bin/initdb -D /data/$PGDIR && \
-sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /data/$PGDIR start && \
+export  PGDATA=/postgresdata  && \
+sudo -u postgres /usr/lib/postgresql/11/bin/initdb -D /postgresdata && \
+sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /postgresdata start && \
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
 sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
 useradd -m -p password1234 nominatim && \
 chown -R nominatim:nominatim ./src && \
 sudo -u nominatim ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
-sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /data/$PGDIR stop && \
-sudo chown -R postgres:postgres /data/$PGDIR
+sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /postgresdata stop && \
+sudo chown -R postgres:postgres /postgresdata
